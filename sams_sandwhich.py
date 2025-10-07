c1c8f07fc153b34ee5d2e1ab79566b12c8f131e3
Alex5B <alexb.22049@obhs.school.nz>
2025-10-07 13:24:14 +1300

    Finishing off achieved

--- sams_sandwhich.py
+++ sams_sandwhich.py
@@ -1,4 +1,5 @@
 #sams sandwhich
+import datetime
 
 def bread_selection(): #lets user select bread
     bread_list = ["White","Brown","Italian","Flatbread"]
@@ -30,6 +31,24 @@ def cheese_selection(): #lets user select cheese
     cheese_selected=int(input("Which cheese did you want? Enter a number: "))
     return cheese_list[cheese_selected-1]
 
+def salads_selection():
+    salad_list = ["Lettuce","Tomato","Carrot","Cucumber","Onions"]
+    count = 0
+    print("We have the following salads, you can have as many as you want")
+    while count <len(salad_list):
+        print(count+1," ",salad_list[count])
+        count +=1
+    print("Press ENTER when you have finished chosing your salads")
+    salads_added = "" #will hold a string of more than one item
+    selected_salad = " " #prompts the user to enter a number in to select a salad
+
+    while selected_salad != "":
+        selected_salad = input(f"What number salad do you want?\nYou have selected: {salads_added}")
+        if selected_salad != "": #if you press enter the statement wont run
+            selected_salad = int(selected_salad)
+            #this variable keeps adding on each selected item from salad list
+            salads_added = salads_added + " " + salad_list[selected_salad-1]
+    return salads_added.strip() #removes empty space at start of the string
 
 
 def dressing_selection(): #lets user select dressing/sauce
@@ -42,14 +61,44 @@ def dressing_selection(): #lets user select dressing/sauce
     dressing_selected=int(input("Which dressing did you want? Enter a number: "))
     return dressing_list[dressing_selected-1]
 
+def output_textfile(first_name,cell_phone,sandwhich_order):
+    date_time=datetime.datetime.now()
+    outF=open("sams_sandwhich.txt","a")#opening up a new file
+    print(f"\n*** Order for {first_name} - {cell_phone}: ***")
+    outFile.write(f"\nDate of booking:{date_time}")
+    for item in sandwhich_order:
+        print(item)#printing each item in the list to the console
+        outFile.write(f"\nEnd of order: {date_time}")
+    print(f"***End of order: {date_time} ***")
+    outFile.write("\n")
+    outFile.write("\n")
+    outFile.write()#closes off text file
+    #once the file prints, it goes back to the menu
+
+
+
 #main program
-print("Welcome to Sam's Sandwich Shop!")
+first_name=str(input("What is your first name?"))
+cell_phone=str(input("What is your cellphone number?"))
 bread_choice=bread_selection()
 meat_choice=meat_selection()
 cheese_choice=cheese_selection()
+salad_choice=salads_selection()
 dressing_choice=dressing_selection()
+
+sandwhich_order=[] #empty list to append order to
+
+sandwhich_order.append(first_name)
+sandwhich_order.append(cell_phone)
+sandwhich_order.append(f"Bread: {bread_choice}")
+sandwhich_order.append(f"Meat: {meat_choice}")
+sandwhich_order.append(f"Cheese: {cheese_choice}")
+sandwhich_order.append(f"Salad  choice: {salad_choice}")
+sandwhich_order.append(f"Dressing choice: {dressing_choice}")
+output_textfile(first_name,cell_phone,sandwhich_order)
 
 print(f"Your selected bread: {bread_choice}")
 print(f"Your selected meat: {meat_choice}")
 print(f"Your selected cheese: {cheese_choice}")
+print(f"Your selected salad(s): {salad_choice}")
 print(f"Your selected dressing: {dressing_choice}")
